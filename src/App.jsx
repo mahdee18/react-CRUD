20ced8bc5575c45ba3e2653b0ad2c0f905debab8
mahdee18 <mahdeec10@gmail.com>
2023-07-17 18:05:35 +0600

    Initial Project set-up

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-red-300 text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </div>
+  );
+};
+
+export default App;
